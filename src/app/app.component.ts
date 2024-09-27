39e97c32e8b9229a844d00ea7bc616d162a38f10
mmullerx4 <mmullerx4@gmail.com>
2024-09-27 12:10:35 -0400

    Data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-     h3 {
-       color: dodgerblue;
-     },
-     p {
-      color: green;
-     }
-   `]
+  styleUrls: ['./app.component.css']
+  // styles: [`
+  //    h3 {
+  //      color: dodgerblue;
+  //    },
+  //    p {
+  //     color: green;
+  //    }
+  //  `]
 })
 export class AppComponent {
   title = 'wdd430-h3';
+  userName = '';
+
+  // onUpdateUserName(event: Event) {
+  //   this.userName = (<HTMLInputElement>event.target).value; //letting typescript know that the target will be an html element.
+  // }
 }
+
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  allowNewUser = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  
+
+  constructor() {  //method that executed at time created by Angular
+    setTimeout(() => {  //arguments with function body
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
-    
+
   }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+
 
 }
